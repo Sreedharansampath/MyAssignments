3fd527103c026d39a4ea5e9f7d37470aa52697a3
Sreedharansampath <124439496+Sreedharansampath@users.noreply.github.com>
2023-02-04 17:39:58 +0530

    Add files via upload
    day1

---
+++ day1/Mobile.java
@@ -0,0 +1,22 @@
+package Week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobileBrandName="Xiaomi 11 Lite 5G NE";
+		char mobileLogo='M';
+		short noOfMobilePiece=1000;
+		int modelNumber=5;
+		long mobileimeiNumber=23574518785l;
+		float mobilePrice=25000;
+		boolean isDamaged=false;
+		System.out.println("Mobile Brand Name: " +mobileBrandName);
+		System.out.println("Mobile Logo: " +mobileLogo);
+		System.out.println("Mobile No of Mobile Piece: " +noOfMobilePiece);
+		System.out.println("Model Number: " +modelNumber);
+		System.out.println("Model Imei Number: " +mobileimeiNumber);
+		System.out.println("Mobile Price: " +mobilePrice);
+		System.out.println("Is Damaged: " +isDamaged);
+	}
+
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,19 @@
+package Week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		String bikeName="Honda DIO";
+		int noOfWheels=2;
+		short noOfMirrors=2;
+		long ChassisNumber=1866882423523523L;
+		boolean isPunctured=false;
+		double runningKM=7543;
+		System.out.println("Bike Name: " +bikeName);
+		System.out.println("No. of Wheels: " +noOfWheels);
+		System.out.println("No. of Mirrors: " +noOfMirrors);
+		System.out.println("Chassis Number: " +ChassisNumber);
+		System.out.println("Is Punctured: " +isPunctured);
+		System.out.println("Running Kilometer: " +runningKM);
+	}
+
+}
